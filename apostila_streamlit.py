7c7b181d52cbaca7514bda945c6bc11aa5c64f8f
leticiaborgescamago <borgeesleticia@gmail.com>
2026-09-04 19:03:00 +0000

    Segunda alteração

--- apostila_streamlit.py
+++ apostila_streamlit.py
@@ -18,3 +18,17 @@ st.sidebar.image("https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w
 st.sidebar.title("Navegação Lateral")
 st.sidebar.info("Tudo que usar 'st.sidebar' vem parar aqui neste menu escuro!")
 
+# =====================================================================
+# 2. TEXTOS E TIPOGRAFIA
+# =====================================================================
+# st.expander cria uma caixa que abre e fecha para organizar a tela
+with st.expander("📝 1. TEXTOS E TIPOGRAFIA (Clique para abrir)", expanded=True):
+    st.title("Isso é um st.title() - Título Gigante")
+    st.header("Isso é um st.header() - Subtítulo Grande")
+    st.subheader("Isso é um st.subheader() - Tópico")
+    
+    st.markdown("**Texto em Negrito** feito com st.markdown()")
+    st.markdown("*Texto em Itálico* feito com st.markdown()")
+    
+    st.write("O st.write() é o coringa! Ele escreve textos normais e substitui o antigo print().")
+    
